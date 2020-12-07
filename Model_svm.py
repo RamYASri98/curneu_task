05c2425105bd4d50b431daecb89e78240fb6b54b
RamYASri98 <41253198+RamYASri98@users.noreply.github.com>
2020-12-08 00:22:35 +0530

    Update Model_svm.py

--- Model_svm.py
+++ Model_svm.py
@@ -1,6 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
+import csv
 
 datasets = pd.read_csv('Social_Network_Ads.csv')
 X = datasets.iloc[:, [2,3]].values
@@ -22,8 +23,10 @@ Y_Pred = classifier.predict(X_Test)
 
 from sklearn.metrics import confusion_matrix
 cm = confusion_matrix(Y_Test, Y_Pred)
+cm
 
 from matplotlib.colors import ListedColormap
+
 X_Set, Y_Set = X_Train, Y_Train
 X1, X2 = np.meshgrid(np.arange(start = X_Set[:, 0].min() - 1, stop = X_Set[:, 0].max() + 1, step = 0.01),
                      np.arange(start = X_Set[:, 1].min() - 1, stop = X_Set[:, 1].max() + 1, step = 0.01))
@@ -40,19 +43,3 @@ plt.ylabel('Estimated Salary')
 plt.legend()
 plt.show()
 
-from matplotlib.colors import ListedColormap
-X_Set, Y_Set = X_Test, Y_Test
-X1, X2 = np.meshgrid(np.arange(start = X_Set[:, 0].min() - 1, stop = X_Set[:, 0].max() + 1, step = 0.01),
-                     np.arange(start = X_Set[:, 1].min() - 1, stop = X_Set[:, 1].max() + 1, step = 0.01))
-plt.contourf(X1, X2, classifier.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),
-             alpha = 0.75, cmap = ListedColormap(('red', 'green')))
-plt.xlim(X1.min(), X1.max())
-plt.ylim(X2.min(), X2.max())
-for i, j in enumerate(np.unique(Y_Set)):
-    plt.scatter(X_Set[Y_Set == j, 0], X_Set[Y_Set == j, 1],
-                c = ListedColormap(('red', 'green'))(i), label = j)
-plt.title('Problem 1')
-plt.xlabel('Age')
-plt.ylabel('Estimated Salary')
-plt.legend()
-plt.show()
